7527fc49ace811b52a8b5aad0458f2258d7ed84c
roysa <roysapeguero@outlook.com>
2022-12-15 16:55:22 -0500

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const { restoreUser } = require("../../utils/auth.js");
+
+router.use(restoreUser);
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
