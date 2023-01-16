3dcd66907501a6331f34be008775511b933b8f97
roysa <roysapeguero@outlook.com>
2023-01-16 04:06:08 -0500

    login modal

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -10,21 +12,24 @@ function Navigation({ isLoaded }){
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <li className='right-navbar'>
+      <li>
         <ProfileButton user={sessionUser} />
       </li>
     );
   } else {
     sessionLinks = (
-      <li className='login-signup-links'>
-        <NavLink to="/login">Log In</NavLink>
+      <li>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
         <NavLink to="/signup">Sign Up</NavLink>
       </li>
     );
   }
 
   return (
-    <ul className="nav-bar">
+    <ul>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
