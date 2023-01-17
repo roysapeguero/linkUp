52547da3d0184fbc0dfe69a8a5a6acf7fc057310
roysa <roysapeguero@outlook.com>
2023-01-16 22:07:04 -0500

    css for login modal

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -25,35 +25,49 @@ function LoginFormModal() {
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
-    </>
+    <div className="modal-container">
+      <div className="modal-form-container">
+        <h1 className="modal-form-title">Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <label className="input-label">
+            Username or Email
+            <input
+              className="input-item"
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          <label className="input-label">
+            Password
+            <input
+              className="input-item"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button className="button" type="submit">Log In</button>
+          <button
+            className="button"
+            type="submit"
+            onClick={(e) => {
+              setCredential("demo@user.io");
+              setPassword("password");
+            }}
+          >
+            Demo User
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
