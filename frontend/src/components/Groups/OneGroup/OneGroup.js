0b8301c4cda514024177eee5ef6eb645e4bee8d6
roysa <roysapeguero@outlook.com>
2023-01-23 04:30:35 -0500

    fix more css

--- frontend/src/components/Groups/OneGroup/OneGroup.js
+++ frontend/src/components/Groups/OneGroup/OneGroup.js
@@ -38,35 +38,38 @@ export default function OneGroupPage (){
   if (currentGroup.numMembers === 0) currentGroup.numMembers = 1
 
   const groupInfo = currentGroup ? (
-    <div className="one-group-container">
-      <div className='one-group-image-container'>
-        <img
-            className='one-group-image'
-            alt={`${currentGroup.name}'s preview`}
-            src={currentGroup.GroupImages.length > 0 ?
-                `${currentGroup.GroupImages[0].url}`
-                : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
-              }
-        />
+    <div className="one-group-page">
+      <div className="line"></div>
+      <div className="one-group-container">
+        <div className='one-group-image-container'>
+          <img
+              className='one-group-image'
+              alt={`${currentGroup.name}'s preview`}
+              src={currentGroup.GroupImages.length > 0 ?
+                  `${currentGroup.GroupImages[0].url}`
+                  : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
+                }
+          />
+        </div>
+        <div className='one-group-description'>
+          <h1 className='one-group-name'>{currentGroup.name}</h1>
+          <div className="one-group-info">
+            <img className="location-img" alt='location icon' src='https://secure.meetupstatic.com/next/images/design-system-icons/map-marker-outline.svg' />
+            <p className='one-group-location'>{`${currentGroup.city}, ${currentGroup.state}`}</p>
+            <p className='one-group-info-members'>{
+              `${currentGroup.numMembers} ${currentGroup.numMembers > 1 ? "members" : "member"} `}&#x2022;
+              {currentGroup.private ? " Private group" : " Public group"}
+            </p>
+            <img className="users-img" alt='users icon' src='https://icons.iconarchive.com/icons/custom-icon-design/silky-line-user/512/users-icon.png' />
+            <img className='user-img' alt='user icon' src='https://www.citypng.com/public/uploads/small/3163494658960sph4obyj9nhmdtvj6hp43eehbjake40kkqawxnvpr4kvh4qv2yxsfxmbf2ahxtgdsjwao1bftyicsrks0diqz1jkk2bokzqtvy.png' />
+            <p className="organized">Organized by </p>
+            <p className="org-user-name">{`${currentGroup.Organizer.firstName} ${currentGroup.Organizer.lastName}`}</p>
+            <h3 className="about-title">What we're about</h3>
+            <p className='one-group-about'>{currentGroup.about}</p>
+          </div>
+        </div>
       </div>
-      <div className='one-group-description'>
-        <h1 className='one-group-name'>{currentGroup.name}</h1>
-      </div>
-      <div className="one-group-info">
-        <img className="location-img" alt='location icon' src='https://secure.meetupstatic.com/next/images/design-system-icons/map-marker-outline.svg' />
-        <p className='one-group-location'>{`${currentGroup.city}, ${currentGroup.state}`}</p>
-        <p className='one-group-info-members'>{
-          `${currentGroup.numMembers} ${currentGroup.numMembers > 1 ? "members" : "member"} `}&#x2022;
-           {currentGroup.private ? " Private group" : " Public group"}
-        </p>
-        <img className="users-img" alt='users icon' src='https://icons.iconarchive.com/icons/custom-icon-design/silky-line-user/512/users-icon.png' />
-        <img className='user-img' alt='user icon' src='https://www.citypng.com/public/uploads/small/3163494658960sph4obyj9nhmdtvj6hp43eehbjake40kkqawxnvpr4kvh4qv2yxsfxmbf2ahxtgdsjwao1bftyicsrks0diqz1jkk2bokzqtvy.png' />
-        <p className="organized">Organized by </p>
-        <p className="org-user-name">{`${currentGroup.Organizer.firstName} ${currentGroup.Organizer.lastName}`}</p>
-        <h3 className="about-title">What we're about</h3>
-        <p className='one-group-about'>{currentGroup.about}</p>
-      </div>
-      <div>
+      <div className="user-btns">
         {isOrganizer && (
         <div className="user-actions">
           <OpenModalButton
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 function SplashPage() {
   return (
     <div className="splash-page-container">
-      <div className="splash-page-content">
         <img className="red-blob" src="https://secure.meetupstatic.com/next/images/blobs/red-blob.svg" alt='red blob' />
         <img className="yellow-blob" src="https://secure.meetupstatic.com/next/images/blobs/yellow-blob.svg" alt='yellow blob' />
         <img className="green-blob" src="https://secure.meetupstatic.com/next/images/blobs/green-blob.svg" alt='green blob' />
+      <div className="splash-page-content">
         <div className="page-left">
           <h1 className="page-title">The fake platform that makes fake planning simple</h1>
           <p className="page-description">
