75466ea0611ece9fba1a640e15607be4dc0a8fbc
roysa <roysapeguero@outlook.com>
2023-01-23 03:18:46 -0500

    responsive one event page

--- frontend/src/components/Events/OneEvent/index.js
+++ frontend/src/components/Events/OneEvent/index.js
@@ -49,55 +49,59 @@ export default function OneEventPage () {
 
   const eventInfo = currentEvent ? (
     <div className="one-event-page">
-    <div className="one-event-container">
-      <div className="name-info">
-        <h1 className="one-event-group-name">{`${currentEvent.name}`}</h1>
-        <div className="circle">
-          <p>{currentEvent.Group.Organizer.firstName[0]}</p>
+      <div className="one-event-container">
+        <div className="name-info">
+          <h1 className="one-event-group-name">{`${currentEvent.name}`}</h1>
+          <div className="circle">
+            <p>{currentEvent.Group.Organizer.firstName[0]}</p>
+          </div>
+          <p className="hosted">Hosted By </p>
+          <p className="hoster-name">{currentEvent.Group.Organizer.firstName}</p>
         </div>
-        <p className="hosted">Hosted By </p>
-        <p className="hoster-name">{currentEvent.Group.Organizer.firstName}</p>
-      </div>
-      <div className="content">
-        <div className="image-container">
-          <img
-            className="event-image"
-            alt={`${currentEvent.name}'s preview`}
-            src={currentEvent.EventImages.length > 0 ?
-              `${currentEvent.EventImages[0].url}`
-              : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
-            }
-          />
-        </div>
-        <div className="one-event-group-details">
-          <img className="one-event-group-img" alt="event's group preview" src={currentEvent.groupImg} />
-          <h4 className="name-group">{currentEvent.Group?.name}</h4>
-          <p className="pub-priv">{currentEvent.private ? `Private Group` : `Public Group`}</p>
-        </div>
-        <div className="one-event-description">
-          <div className="event-specifics">
-            <img className='time-img' alt='time icon' src='https://secure.meetupstatic.com/next/images/design-system-icons/time-outline.svg' />
-            <p className="date-times">
-              {`${dMD} at ${newStartDateTime.slice(0,-6)} ${newStartDateTime.slice(-2)} to
-              ${eDMD} at ${newEndDateTime.slice(0,-6)} ${newEndDateTime.slice(-2)} EST`}
-            </p>
-            <img className="type-img" alt='type icon' src={currentEvent.type === 'In person' ? "https://secure.meetupstatic.com/next/images/design-system-icons/map-marker-outline.svg" :
-            'https://secure.meetupstatic.com/next/images/design-system-icons/video-outline.svg'} />
-            <p className="one-event-type">{currentEvent.type} Event</p>
+        <div className="content">
+          <div className="one-event-image-container">
+            <img
+              className="event-image"
+              alt={`${currentEvent.name}'s preview`}
+              src={currentEvent.EventImages.length > 0 ?
+                `${currentEvent.EventImages[0].url}`
+                : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
+              }
+            />
+          </div>
+          <div className="right-side">
+            <div className="one-event-group-details">
+              <img className="one-event-group-img" alt="event's group preview" src={currentEvent.groupImg} />
+              <h4 className="name-group">{currentEvent.Group?.name}</h4>
+              <p className="pub-priv">{currentEvent.private ? `Private Group` : `Public Group`}</p>
+            </div>
+            <div className="one-event-description">
+              <div className="event-specifics">
+                <img className='time-img' alt='time icon' src='https://secure.meetupstatic.com/next/images/design-system-icons/time-outline.svg' />
+                <p className="date-times">
+                  {`${dMD} at ${newStartDateTime.slice(0,-6)} ${newStartDateTime.slice(-2)} to
+                  ${eDMD} at ${newEndDateTime.slice(0,-6)} ${newEndDateTime.slice(-2)} EST`}
+                </p>
+                <img className="type-img" alt='type icon' src={currentEvent.type === 'In person' ? "https://secure.meetupstatic.com/next/images/design-system-icons/map-marker-outline.svg" :
+                'https://secure.meetupstatic.com/next/images/design-system-icons/video-outline.svg'} />
+                <p className="one-event-type">{currentEvent.type} Event</p>
+              </div>
+              {isOrganizer && (
+                <button className="delete-event" onClick={(e) => handleDelete(e, currentEvent.id)}>
+                  Delete Event
+                </button>
+              )}
+            </div>
           </div>
           <div>
-            <h2 className="detail-title">Details</h2>
-            <p className="event-details">{currentEvent.description}</p>
-            <h2 className="attendees">{`Attendees(${currentEvent.numAttending})`}</h2>
+            <div className="detail-div">
+              <h2 className="detail-title">Details</h2>
+              <p className="event-details">{currentEvent.description}</p>
+              <h2 className="attendees">{`Attendees(${currentEvent.numAttending})`}</h2>
+            </div>
           </div>
         </div>
-        {isOrganizer && (
-          <button className="delete-event" onClick={(e) => handleDelete(e, currentEvent.id)}>
-            Delete Event
-          </button>
-        )}
       </div>
-    </div>
     </div>
   ) : (<h1>Loading event details</h1>)
   return eventInfo
