128d999f3e68f0dd4a6fb6529ddb9a57f044e588
roysa <roysapeguero@outlook.com>
2023-03-27 00:37:44 -0400

    one group

--- frontend/src/components/Groups/OneGroup/OneGroup.js
+++ frontend/src/components/Groups/OneGroup/OneGroup.js
@@ -39,7 +39,6 @@ export default function OneGroupPage (){
 
   const groupInfo = currentGroup ? (
     <div className="one-group-page">
-      <div className="line"></div>
       <div className="one-group-container">
         <div className='one-group-image-container'>
           <img
@@ -54,21 +53,33 @@ export default function OneGroupPage (){
         <div className='one-group-description'>
           <h1 className='one-group-name'>{currentGroup.name}</h1>
           <div className="one-group-info">
-            <img className="location-img" alt='location icon' src='https://secure.meetupstatic.com/next/images/design-system-icons/map-marker-outline.svg' />
-            <p className='one-group-location'>{`${currentGroup.city}, ${currentGroup.state}`}</p>
-            <p className='one-group-info-members'>{
-              `${currentGroup.numMembers} ${currentGroup.numMembers > 1 ? "members" : "member"} `}&#x2022;
-              {currentGroup.private ? " Private group" : " Public group"}
-            </p>
-            <img className="users-img" alt='users icon' src='https://icons.iconarchive.com/icons/custom-icon-design/silky-line-user/512/users-icon.png' />
-            <img className='user-img' alt='user icon' src='https://www.citypng.com/public/uploads/small/3163494658960sph4obyj9nhmdtvj6hp43eehbjake40kkqawxnvpr4kvh4qv2yxsfxmbf2ahxtgdsjwao1bftyicsrks0diqz1jkk2bokzqtvy.png' />
-            <p className="organized">Organized by </p>
-            <p className="org-user-name">{`${currentGroup.Organizer.firstName} ${currentGroup.Organizer.lastName}`}</p>
-            <h3 className="about-title">What we're about</h3>
-            <p className='one-group-about'>{currentGroup.about}</p>
+            <div className="og-about-info">
+              <img className="location-img" alt='location icon' src='https://secure.meetupstatic.com/next/images/design-system-icons/map-marker-outline.svg' />
+              <p className='one-group-location'>{`${currentGroup.city}, ${currentGroup.state}`}</p>
+            </div>
+            <div className="og-about-info">
+              <img className="users-img" alt='users icon' src='https://icons.iconarchive.com/icons/custom-icon-design/silky-line-user/512/users-icon.png' />
+              <p className='one-group-info-members'>{
+                `${currentGroup.numMembers} ${currentGroup.numMembers > 1 ? "members" : "member"} `}&#x2022;
+                {currentGroup.private ? " Private group" : " Public group"}
+              </p>
+            </div>
+            <div className="og-about-info">
+              <img className='user-img' alt='user icon' src='https://www.citypng.com/public/uploads/small/3163494658960sph4obyj9nhmdtvj6hp43eehbjake40kkqawxnvpr4kvh4qv2yxsfxmbf2ahxtgdsjwao1bftyicsrks0diqz1jkk2bokzqtvy.png' />
+              <p className="organized">Organized by <strong>{`${currentGroup.Organizer.firstName} ${currentGroup.Organizer.lastName}`}</strong></p>
+              {/* <p className="org-user-name">{`${currentGroup.Organizer.firstName} ${currentGroup.Organizer.lastName}`}</p> */}
+            </div>
+
           </div>
         </div>
       </div>
+        <div className="one-group-tabs">
+
+        </div>
+        <div>
+          <h3 className="about-title">What we're about</h3>
+          <p className='one-group-about'>{currentGroup.about}</p>
+        </div>
       <div className="user-btns">
         {isOrganizer && (
         <div className="user-actions">
