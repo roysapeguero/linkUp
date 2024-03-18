4602a792d0f4757a49c7ddcf4f495da4a36d52db
roysa <roysapeguero@outlook.com>
2024-03-17 23:04:50 -0700

    add longer event descriptions

--- backend/db/seeders/20221219223632-create-events-for-events-table.js
+++ backend/db/seeders/20221219223632-create-events-for-events-table.js
@@ -16,7 +16,8 @@ module.exports = {
           venueId: 1,
           groupId: 1,
           name: "Bake Off!",
-          description: "Join us for our annual bake off!",
+          description:
+            "Indulge your sweet tooth and join us for our annual Bake Off extravaganza! Whether you're a seasoned baker or a novice, this event is perfect for unleashing your creativity in the kitchen. Show off your signature recipes, exchange baking tips, and compete for the coveted title of Bake Off Champion. From decadent cakes to delicate pastries, let your culinary skills shine and satisfy your craving for confectionery delights!",
           type: "In person",
           capacity: 20,
           price: 10.95,
@@ -28,7 +29,7 @@ module.exports = {
           groupId: 2,
           name: "Purrcation!",
           description:
-            "Its here! Our group cat retreat. Bring your friendly furry friends and have a PAWSOME time!",
+            "Calling all cat lovers! Treat yourself and your furry feline friends to the ultimate Purrcation getaway. Join us for a week-long retreat filled with pampering, playtime, and purrs! From cozy cat naps to interactive toy sessions, this is the purrfect opportunity to bond with your beloved pets and fellow cat enthusiasts. Don't miss out on this PAWSOME adventure!",
           type: "In person",
           capacity: 30,
           price: 10.0,
@@ -40,7 +41,7 @@ module.exports = {
           groupId: 3,
           name: "Ice Party",
           description:
-            "Ice cream in the winter? Gather with your fellow ice cream lovers for some fun in the sun?",
+            "Beat the heat and chill out with us at the Ice Party! Embrace the winter wonderland vibes and indulge in all things ice cream. From classic flavors to exotic creations, our Ice Party has something for every palate. Enjoy scoops of creamy goodness, participate in fun ice cream-themed activities, and bask in the sun with fellow frozen treat enthusiasts. It's a cool way to create sweet memories!",
           type: "In person",
           capacity: 50,
           price: 10.95,
@@ -52,7 +53,7 @@ module.exports = {
           groupId: 4,
           name: "Bingination",
           description:
-            "Weekly watch party for our group selected season favorite!",
+            "Get ready to binge-watch your favorite shows with us at Bingination! Join our weekly watch party where we dive into the latest and greatest TV series. From gripping dramas to laugh-out-loud comedies, we've got something for everyone. Grab your popcorn, cozy up on the couch, and get ready for hours of non-stop entertainment!",
           type: "Online",
           capacity: 40,
           price: 10.0,
@@ -63,7 +64,8 @@ module.exports = {
           venueId: 1,
           groupId: 1,
           name: "Cake Masterclass",
-          description: "Learn the art of baking cakes from the experts!",
+          description:
+            "Unlock the secrets of baking perfection at our Cake Masterclass! Join us for an immersive culinary experience led by expert pastry chefs. From mastering the art of cake decoration to perfecting the science of baking, this class covers it all. Whether you're a beginner or a seasoned baker, come expand your skills and indulge in a slice of sweet success!",
           type: "In person",
           capacity: 15,
           price: 20.0,
@@ -75,7 +77,7 @@ module.exports = {
           groupId: 2,
           name: "Cat Yoga Session",
           description:
-            "Relax and stretch with your feline friends in a calming yoga session.",
+            "Find your zen with our Cat Yoga Session! Join us for a purrfectly relaxing experience as we stretch and unwind surrounded by adorable feline companions. Whether you're a seasoned yogi or a first-timer, this session offers a unique opportunity to deepen your practice while bonding with our furry friends. Let the soothing purrs and gentle stretches melt away your stress!",
           type: "In person",
           capacity: 20,
           price: 15.0,
@@ -87,7 +89,7 @@ module.exports = {
           groupId: 3,
           name: "Ice Cream Social",
           description:
-            "Indulge in a variety of delicious ice cream flavors with fellow enthusiasts.",
+            "Join us for an Ice Cream Social extravaganza! Indulge your sweet tooth with a tantalizing array of frozen delights, from classic sundaes to artisanal gelato. Whether you're a chocolate aficionado or a fruit sorbet enthusiast, there's something for everyone to enjoy. Grab a spoon, mingle with fellow ice cream enthusiasts, and savor every delicious moment!",
           type: "In person",
           capacity: 30,
           price: 12.5,
@@ -98,7 +100,8 @@ module.exports = {
           venueId: null,
           groupId: 4,
           name: "Anime Movie Marathon",
-          description: "Join us for a marathon of your favorite anime films!",
+          description:
+            "Gear up for an epic Anime Movie Marathon! Join us for a cinematic journey through your favorite anime films, from timeless classics to the latest releases. Whether you're a die-hard otaku or a casual fan, this marathon promises non-stop action, drama, and excitement. Grab your popcorn, settle in, and immerse yourself in the colorful worlds of anime!",
           type: "Online",
           capacity: 50,
           price: 0.0,
@@ -110,7 +113,7 @@ module.exports = {
           groupId: 5,
           name: "Classic Movie Night",
           description:
-            "Enjoy a screening of classic movies with popcorn and friends.",
+            "Step back in time and join us for Classic Movie Night! Experience the golden age of cinema with screenings of beloved films from Hollywood's heyday. From timeless romances to thrilling adventures, our movie night offers something for every film buff. Grab your popcorn, gather your friends, and enjoy an evening of cinematic nostalgia under the stars!",
           type: "In person",
           capacity: 25,
           price: 8.0,
@@ -122,7 +125,7 @@ module.exports = {
           groupId: 1,
           name: "Fruit Cake Tasting",
           description:
-            "Sample a variety of delicious fruit cakes baked by our members.",
+            "Join us for a mouthwatering Fruit Cake Tasting event! Sample a delectable assortment of fruit-infused cakes crafted by our talented members. From zesty lemon cakes to rich berry delights, explore a symphony of flavors that will tantalize your taste buds. Whether you're a fruit cake connoisseur or simply curious, this tasting experience is sure to delight!",
           type: "In person",
           capacity: 20,
           price: 10.0,
@@ -133,7 +136,8 @@ module.exports = {
           venueId: null,
           groupId: 2,
           name: "Cat Adoption Fair",
-          description: "Find your purrfect companion at our cat adoption fair!",
+          description:
+            "Find your purrfect companion at our Cat Adoption Fair! Join us for a day of feline fun as we showcase adorable cats in need of loving homes. Whether you're looking for a playful kitten or a mellow senior cat, our adoption fair offers a wide selection of furry friends waiting to steal your heart. Come meet your new best friend and give a cat a forever home!",
           type: "In person",
           capacity: 50,
           price: 0.0,
@@ -145,7 +149,7 @@ module.exports = {
           groupId: 3,
           name: "DIY Ice Cream Workshop",
           description:
-            "Learn to make your own delicious ice cream flavors at home!",
+            "Get ready to churn up some delicious fun at our DIY Ice Cream Workshop! Join us as we explore the art of making homemade ice cream from scratch. From selecting the finest ingredients to mastering the perfect churn, this hands-on workshop will teach you everything you need to know to create creamy, dreamy treats in your own kitchen. Say goodbye to store-bought pints and hello to homemade happiness!",
           type: "In person",
           capacity: 15,
           price: 25.0,
@@ -156,7 +160,8 @@ module.exports = {
           venueId: null,
           groupId: 4,
           name: "Anime Trivia Night",
-          description: "Test your anime knowledge with our fun trivia night!",
+          description:
+            "Test your anime knowledge at our Anime Trivia Night! Join fellow fans for a fun-filled evening of friendly competition and otaku camaraderie. From classic series to obscure references, our trivia night covers it all. Put your memory to the test, earn bragging rights, and prove yourself as the ultimate anime aficionado!",
           type: "Online",
           capacity: 30,
           price: 5.0,
@@ -168,7 +173,7 @@ module.exports = {
           groupId: 5,
           name: "Movie Discussion Club",
           description:
-            "Engage in lively discussions about your favorite movies and directors.",
+            "Join our Movie Discussion Club and dive into the world of cinema! Whether you're a film buff or a casual viewer, our club offers a welcoming space to share your thoughts, insights, and opinions on a wide range of movies. Engage in lively discussions, discover hidden gems, and connect with fellow movie enthusiasts. Grab your popcorn and get ready for cinematic conversations!",
           type: "In person",
           capacity: 20,
           price: 0.0,
@@ -180,7 +185,7 @@ module.exports = {
           groupId: 1,
           name: "Holiday Baking Extravaganza",
           description:
-            "Get into the festive spirit with our holiday-themed baking competition!",
+            "Get into the festive spirit with our Holiday Baking Extravaganza! Join us for a seasonal celebration of sweet treats and merry memories. From gingerbread houses to festive fruitcakes, our baking competition is a showcase of holiday cheer and culinary creativity. Bring your best recipes, don your aprons, and let the baking begin!",
           type: "In person",
           capacity: 25,
           price: 15.0,
@@ -192,7 +197,7 @@ module.exports = {
           groupId: 2,
           name: "Cat Costume Contest",
           description:
-            "Dress up your cats in their most adorable costumes and win prizes!",
+            "Dress to impress at our Cat Costume Contest! Show off your feline's flair for fashion as they strut their stuff in adorable costumes. From elegant princesses to daring superheroes, let your cat's personality shine on the runway. Prizes await the most creative and captivating costumes, so don't miss out on this chance to showcase your kitty's style!",
           type: "In person",
           capacity: 20,
           price: 10.0,
@@ -204,7 +209,7 @@ module.exports = {
           groupId: 3,
           name: "Ice Cream Making Class",
           description:
-            "Learn the secrets of making creamy, delicious ice cream from scratch.",
+            "Discover the sweet science of ice cream making at our hands-on workshop! Join us as we delve into the art of creating creamy, dreamy desserts from scratch. From mastering the perfect custard base to experimenting with flavor combinations, this class offers a deliciously educational experience for ice cream enthusiasts of all ages. Get ready to churn up some frozen fun!",
           type: "In person",
           capacity: 15,
           price: 20.0,
@@ -215,7 +220,8 @@ module.exports = {
           venueId: null,
           groupId: 4,
           name: "Anime Karaoke Night",
-          description: "Sing your favorite anime theme songs with fellow fans!",
+          description:
+            "Sing your heart out at our Anime Karaoke Night! Join fellow fans for a night of musical mayhem as we belt out our favorite anime theme songs. From catchy openings to emotional endings, our karaoke playlist has something for everyone. Grab the mic, unleash your inner idol, and let the music transport you to the colorful world of anime!",
           type: "Online",
           capacity: 20,
           price: 7.0,
@@ -227,7 +233,7 @@ module.exports = {
           groupId: 5,
           name: "Movie Marathon Weekend",
           description:
-            "Join us for a weekend of non-stop movie watching and popcorn munching!",
+            "Grab your popcorn and settle in for a Movie Marathon Weekend! Join us for two days of non-stop cinematic fun as we screen a selection of blockbuster hits, cult classics, and hidden gems. Whether you're a cinephile or a casual viewer, our movie marathon offers something for everyone. Sit back, relax, and immerse yourself in a weekend of movie magic!",
           type: "In person",
           capacity: 30,
           price: 0.0,
@@ -238,7 +244,8 @@ module.exports = {
           venueId: null,
           groupId: 1,
           name: "Spring Baking Showcase",
-          description: "Showcase your baking skills with spring-themed treats!",
+          description:
+            "Celebrate the arrival of spring with our Baking Showcase event! Join us as we showcase a vibrant array of seasonal treats, from floral-inspired cakes to fresh fruit tarts. Whether you're a baking enthusiast or simply looking to satisfy your sweet tooth, our showcase promises a feast for the senses. Embrace the spirit of spring and indulge in a symphony of flavors!",
           type: "In person",
           capacity: 20,
           price: 12.0,
@@ -250,7 +257,7 @@ module.exports = {
           groupId: 2,
           name: "Cat Cafe Social",
           description:
-            "Relax with coffee and cats at our cozy cat cafe meet-up!",
+            "Join us for a purrfectly delightful Cat Cafe Social! Relax with a cup of coffee or tea in the company of our resident feline friends. Whether you're a cat lover looking to unwind or simply seeking a cozy spot to enjoy a warm beverage, our cafe offers a tranquil escape from the hustle and bustle of everyday life. Come for the cats, stay for the purrs!",
           type: "In person",
           capacity: 25,
           price: 10.0,
@@ -262,7 +269,7 @@ module.exports = {
           groupId: 3,
           name: "Ice Cream Festival",
           description:
-            "Indulge in a variety of ice cream flavors at our summer festival!",
+            "Satisfy your sweet tooth at our Ice Cream Festival extravaganza! Join us for a celebration of all things creamy and delicious, from artisanal gelato to indulgent sundaes. Whether you prefer classic flavors or daring new creations, our festival offers a tantalizing array of frozen treats to tantalize your taste buds. Come beat the heat and enjoy a scoop of summertime happiness!",
           type: "In person",
           capacity: 50,
           price: 15.0,
@@ -274,7 +281,7 @@ module.exports = {
           groupId: 4,
           name: "Anime Art Showcase",
           description:
-            "Display your anime-inspired artwork and mingle with fellow artists!",
+            "Calling all anime artists! Showcase your talents at our Anime Art Showcase event. Whether you're a seasoned illustrator or a budding creator, our showcase offers a platform to share your work with fellow fans. From fan art to original creations, all styles and mediums are welcome. Come mingle with fellow artists, gain inspiration, and celebrate the vibrant world of anime art!",
           type: "Online",
           capacity: 40,
           price: 0.0,
@@ -286,7 +293,7 @@ module.exports = {
           groupId: 5,
           name: "Outdoor Movie Night",
           description:
-            "Watch your favorite movies under the stars at our outdoor screening event!",
+            "Experience the magic of cinema under the stars at our Outdoor Movie Night! Grab a blanket, cozy up with friends, and enjoy a selection of outdoor screenings featuring your favorite films. From romantic comedies to action-packed blockbusters, our movie night offers something for every moviegoer. Don't miss this chance to enjoy an unforgettable evening of al fresco entertainment!",
           type: "In person",
           capacity: 30,
           price: 5.0,
